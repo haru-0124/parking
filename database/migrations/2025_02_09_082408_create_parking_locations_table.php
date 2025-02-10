650548fff2daef50c12e841ea54a48309d5fc17d
haru-0124 <k.haruto0124@au.com>
2025-02-10 02:51:56 +0000

    first commit

---
+++ database/migrations/2025_02_09_082408_create_parking_locations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parking_locations', function (Blueprint $table) {
+            $table->id();
+            $table->double('latitude', 9, 6);
+            $table->double('longitude', 9, 6);
+            $table->foreignId('parking_types_id')->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parking_locations');
+    }
+};
