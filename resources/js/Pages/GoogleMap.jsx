a87b8dda1b0ef72dd885a8fce5e110a1be074c12
haru-0124 <k.haruto0124@au.com>
2025-02-13 05:31:12 +0000

    get_current_location

--- resources/js/Pages/GoogleMap.jsx
+++ resources/js/Pages/GoogleMap.jsx
@@ -1,27 +1,60 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import { useEffect, useState } from "react";
 import {
   APIProvider,
   Map,
-  MapCameraChangedEvent,
 } from "@vis.gl/react-google-maps";
 
 const MyMap = () => {
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      setError("お使いのブラウザから位置情報を入手できませんでした。")
+      return;
+    }
+
+    const watchId = navigator.geolocation.watchPosition(
+      (position) => {
+        setLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        });
+        console.log("現在地：", position.coords.latitude, position.coords.longitude);
+      },
+      (err) => {
+        setError("位置情報の取得に失敗しました。" + err.message);
+      }
+    );
+
+    return () => {
+      navigator.geolocation.clearWatch(watchId);
+    };
+  }, []);
+
   return (
     <APIProvider
       apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
       onLoad={() => console.log("Maps API has loaded.")}
     >
-      <Map
-        style={{ width: "100%", height: "500px" }} // 幅100%、高さ500px
-        defaultZoom={13}
-        defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
-        onCameraChanged={(ev) =>
-          console.log("camera changed:", ev.detail.center, "zoom:", ev.detail.zoom)
-        }
-      >
-      </Map>
-      <h1>Hello World</h1>
+      {error ? (
+        <h2 style={{color: "red"}}>{error}</h2>
+      ) : (
+        location ? (
+          <Map
+          style={{ width: "100%", height: "500px" }} // 幅100%、高さ500px
+          defaultZoom={13}
+          defaultCenter={location}
+          onCameraChanged={(ev) =>
+            console.log("camera changed:", ev.detail.center, "zoom:", ev.detail.zoom)
+          }
+          />
+        ) : (
+          <h2>位置情報を取得中...</h2>
+        )
+      )}
+
     </APIProvider>
   );
 };
